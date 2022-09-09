6a96a685b443d22f0741b28d6eeb17ebd71f5e59
b0b1n <sohaibskious@gmail.com>
2022-09-09 02:06:29 +0200

    Project setup

---
+++ core/src/main/java/maven/game/Main.java
@@ -0,0 +1,11 @@
+package maven.game;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args) {
+        log.info("guess the number game");
+    }
+}
