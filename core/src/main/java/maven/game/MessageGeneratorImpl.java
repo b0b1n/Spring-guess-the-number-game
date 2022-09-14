dc8dc7d57d427859c6bae5ba0ea823c3696a4b0e
b0b1n <sohaibskious@gmail.com>
2022-09-14 04:32:19 +0200

    configured the messageGenerator methods

--- core/src/main/java/maven/game/MessageGeneratorImpl.java
+++ core/src/main/java/maven/game/MessageGeneratorImpl.java
@@ -23,11 +23,29 @@ public class MessageGeneratorImpl implements MessageGenerator {
     // == public method ==
     @Override
     public String getMainMessage() {
-        return "calling method : getMainMessage()";
+        return "number is between "
+                + game.getSmallest()
+                + " and " + game.getBiggest()
+                + " . Can you guess it?";
     }
 
     @Override
     public String getResultMessage() {
-        return "calling method : getResultMessage()";
+
+        if(game.isGameWon()) {
+            return " Congratulations!! You won ... The number you guessed was : " + game.getNumber();
+        } else if (game.isGameLost()) {
+            return "A A AAH!! You lost  :( ... The number  was : " + game.getNumber();
+        } else if (!game.isValidNumberRange()) {
+            return "The number you guessed is out of range :(";
+        } else if (game.getRemainingGuesses() == guessCount) {
+            return " What will your first guess be? ";
+        } else {
+            String direction = "Lower";
+            if (game.getGuess() < game.getNumber()) {
+                direction = "Higher";
+            }
+            return direction + "!! You have : " + game.getRemainingGuesses() + " guesses left .";
+        }
     }
 }
