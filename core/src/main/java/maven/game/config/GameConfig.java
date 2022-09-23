b68f624741f192c0f9810c62e4edb6c074d047ee
b0b1n <sohaibskious@gmail.com>
2022-09-23 15:20:55 +0200

    Using properties

--- core/src/main/java/maven/game/config/GameConfig.java
+++ core/src/main/java/maven/game/config/GameConfig.java
@@ -2,13 +2,18 @@ package maven.game.config;
 
 import maven.game.GuessCount;
 import maven.game.MaxNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:config/game.properties")
 public class GameConfig {
     // == fields ==
-    private int maxNumber = 50;
+    @Value("${game.maxNumber:20}")// Default value is 20 in case game.guessCount wasn't found
+    private int maxNumber;
+    @Value("${game.guessCount:5}") // Default value is 5 in case game.guessCount wasn't found
     private int guessCount = 8;
 
     // == bean methods
